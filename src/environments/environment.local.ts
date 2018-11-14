9d4518c4cb8453016737922af9b90416d0fd50a3
Stefan Janjic <stefanjanjic90@gmail.com>
2018-11-14 18:00:41 +0100

    Add files via upload

---
+++ src/environments/environment.local.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:4000',
+  loginUrl: 'http://localhost:4000/login',
+  logoutUrl: 'http://localhost:4000/logout'
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: '<%= apiBaseUrl %>',
+  loginUrl: '<%= apiBaseUrl %>/login',
+  logoutUrl: '<%= apiBaseUrl %>/logout'
+};
